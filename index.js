602a25cc24ce7c2f12183d6d81156c725535d477
Ej Myles <ejelymyles@gmail.com>
2023-04-26 11:26:57 -0700

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue > 42){
+        return someValue - 42;
+    } else {
+        return 42 - someValue;
+    }
+}
+
+function distanceFromHqInFeet(someValue){
+  const blocks = distanceFromHqInBlocks(someValue);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const distance = destination - start;
+    if (distance >= 0){
+        return distance * 264;
+    } else {
+        return (distance * 264) * -1;
+    }
+}
+
+function calculatesFarePrice (start, destination){
+    const footDistance = distanceTravelledInFeet(start, destination);
+    if (footDistance <= 400){
+        return 0;
+    } else if (footDistance <= 2000){
+        return (footDistance - 400) * 0.02;
+    } else if (footDistance <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
